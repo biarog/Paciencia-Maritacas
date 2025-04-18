9025e9496b352c33356179973178688cc3befd86
biarog <66644264+biarog@users.noreply.github.com>
2025-04-18 14:35:17 -0300

    Mudanças basicas nas cartas

--- scripts/carta.gd
+++ scripts/carta.gd
@@ -1,5 +1,18 @@
-extends Node2D
+extends Control
 
 @export_enum('Ouros', 'Espadas', 'Copas', 'Paus') var casa
 @export_range(1, 13) var valor : int
 @export var imagem : Texture2D
+
+
+func _on_area_carta_mouse_entered():
+	var tween = create_tween()
+	
+	tween.tween_property(self, "scale", Vector2(1.25,1.25), 0.1)
+	
+
+func _on_area_carta_mouse_exited():
+	var tween = create_tween()
+	
+	tween.tween_property(self, "scale", Vector2(1,1), 0.1)
+	
