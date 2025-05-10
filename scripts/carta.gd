f4eb9af577f08675695decd033c6e2d741bce94c
biarog <66644264+biarog@users.noreply.github.com>
2025-05-10 14:24:20 -0300

    Adicionado template das cartas
    Adicionada imagem de template das cartas e também a lógica necessária no corpo da carta para mudar de frame baseado em seu valor e naipe

--- scripts/carta.gd
+++ scripts/carta.gd
@@ -2,7 +2,10 @@ extends Control
 
 @export_enum('Ouros', 'Espadas', 'Copas', 'Paus') var casa
 @export_range(1, 13) var valor : int
-@export var imagem : Texture2D
+
+func _ready():
+	var sprite_carta = $"Sprite Carta"
+	sprite_carta.frame_coords = Vector2((valor-1), casa)
 
 func _on_area_carta_mouse_entered():
 	emit_signal("mouse_entered", self)
