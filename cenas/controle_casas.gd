8647ab5097f496f0fda4c2fcf42f7d14eba023fb
Beatriz Rogers <beatrizrogers55@gmail.com>
2025-06-06 19:08:58 -0300

    Correção de bug

--- cenas/controle_casas.gd
+++ cenas/controle_casas.gd
@@ -8,6 +8,7 @@ var movimento:Movimento_Jogo
 func _ready() -> void:
 	casas = get_tree().get_nodes_in_group("Casas Jogo")
 	movimento.soltouCartaCasa.connect(_normalizar_zindex)
+	movimento.soltouCartaCasa.connect(_atualizar_conexoes)
 	movimento.moveuCartaCasa.connect(_atualizar_conexoes)
 
 func _normalizar_zindex() -> void:
@@ -18,12 +19,12 @@ func _normalizar_zindex() -> void:
 
 func _atualizar_conexoes() -> void:
 	for casa in casas:
-		for i in range(1, casa.get_child_count()):
-			var carta = casa.get_child(i)
-			controle_jogo.desconectar_carta(carta)
-			
-		controle_jogo.conectar_carta(casa.get_child(casa.get_child_count()-1))
-		
+		if casa.get_child_count() > 1:
+			for i in range(1, casa.get_child_count()):
+				var carta = casa.get_child(i)
+				controle_jogo.desconectar_carta(carta)
+				
+			controle_jogo.conectar_carta(casa.get_child(casa.get_child_count()-1))
 	check_vitoria()
 
 
