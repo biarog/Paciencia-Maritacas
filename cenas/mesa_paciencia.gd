7b27aa6d4a1098d2faa918061a052581ef9eb444
Matteo Savan <matteosavan@gmail.com>
2025-06-06 03:02:59 -0300

    menu pausa ok

---
+++ cenas/mesa_paciencia.gd
@@ -0,0 +1,31 @@
+extends Control
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		print("ESC apertado!")
+		if get_tree().paused:
+			_unpause_game()
+		else:
+			_pause_game()
+
+func _pause_game():
+	$MenuPausa.visible = true
+	get_tree().paused = true
+
+func _unpause_game():
+	print("entrou")
+	$MenuPausa.visible = false
+	get_tree().paused = false
+
+func _on_continue_pressed() -> void:
+	print("entrou2")
+	_unpause_game()
+
+func _on_retornar_menu_inicial_pressed() -> void:
+	_unpause_game()
+	get_tree().change_scene_to_file("res://cenas/menu_inicial.tscn")
+
+
+func _on_recomecar_pressed() -> void:
+	_unpause_game()
+	get_tree().change_scene_to_file("res://cenas/mesa_paciencia.tscn")
